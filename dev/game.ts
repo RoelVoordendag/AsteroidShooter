f19df97aafd96e17b1c626a9e801d0a78178f930
RoelVoordendag <rvoordendag@gmail.com>
2017-06-19 12:42:47 +0200

    changed some thing

--- dev/game.ts
+++ dev/game.ts
@@ -7,7 +7,7 @@ class Game {
     private astroid: Astroid;
     private bullet: Bullet;
     public score: number;
-    public div:HTMLElement;
+    private div:HTMLElement;
     private character:character;
     private miniMeteors: miniAstroid;
     private miniAstroid: Array<miniAstroid> = new Array<miniAstroid>();
